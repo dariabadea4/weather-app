b3cd12b1010dac64a24ef20385dc92770906ef73
dariabadea4 <164878313+dariabadea4@users.noreply.github.com>
2024-09-20 21:06:11 +0300

    added js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './scripts/city-input.js'
 import './scripts/city-input__favorites.js'
-import './scripts/quote'
+import './scripts/quote.js'
 import './scripts/widget.js'
 console.log(document.URL.includes('pag-2'));
